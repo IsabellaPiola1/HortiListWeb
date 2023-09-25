0302ee85cfd0ac0eb5907d0a5dac156598182dd8
Isabella Piola <rm93960@fiap.com.br>
2023-09-25 18:44:54 -0300

    delete e update

--- src/app/categorias/[id]/edit/page.jsx
+++ src/app/categorias/[id]/edit/page.jsx
@@ -1,62 +1,15 @@
-"use client"
+import { getConta } from "@/actions/contas";
+import NavBar from "@/components/NavBar";
+import FormEdit from "./Form";
 
-import Button from "@/components/Button";
-import InputText from "@/components/InputText";
-import { CheckIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-import { redirect } from 'next/navigation'
-import { update } from "@/actions/contas";
-
-export default function FormEdit({categorias}){
-    const [message, setMessage] = useState("")
-    const [categoriaEdit, setCategoriaEdit] = useState(categorias)
+export default async function FormContas({params}) {
     
-    async function handleSubmit(){
-        const resp = await update(categoriaEdit)
-        if(resp?.error){
-            setMessage(resp.error)
-            return
-        }
-        redirect("/categorias")
-    }
+    const conta = await getConta(params.id)
 
-    function handleFieldChange(field, value){
-        //console.log(field, value)
-        setCategoriaEdit({
-            ...categoriaEdit,
-            [field]: value
-        })
-    }
-
-    return(
-        <main className="bg-slate-900 mt-10 p-12 rounded-xl max-w-lg m-auto">
-        <h2 className="text-2xl font-bold">Editar Categoria</h2>
-        <form action={handleSubmit}>
-            <InputText 
-                name="nome" 
-                id="nome" 
-                label="nome" 
-                value={categoriaEdit.nome} 
-                onChange={e => handleFieldChange("nome", e.target.value)}
-            />
-            <InputText 
-                name="icone" 
-                id="icone" 
-                label="ícone" 
-                value={categoriaEdit.icone} 
-                onChange={e => handleFieldChange("icone", e.target.value)}
-            />
-
-            <div className="flex justify-around mt-4">
-                <Button href="/categorias" variant="secundary" icon={<ArrowLeftIcon className="h-6 w-6" />}>
-                    cancelar
-                </Button>
-                <Button element="button" icon={<CheckIcon className="h-6 w-6" />}>
-                    salvar
-                </Button>
-            </div>
-        </form>
-        <p>{message}</p>
-    </main>
+    return (
+        <>
+            <NavBar active="categorias" />
+            <FormEdit categorias={categorias} />
+        </>
     )
 }
